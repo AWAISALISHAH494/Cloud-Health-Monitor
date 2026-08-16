456d1b44cc26124a8b791df812fc3f971d09c626
AWAISALISHAH494 <awaisalishah02@gmail.com>
2026-08-16 19:38:51 +0500

    Update to settings .py - hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-_!hwwis*rmzzikc0!jl&7b_%@wsrco53+vc_^ec0q&mrjhtx9y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    "54.90.170.164",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
